a8d2b621559cd1c36a058f35732155898461676a
Gevorg Manukyan <gevorg.manukyan@yahoo.com>
2019-01-16 19:58:04 +0400

    added simple dropdown menu

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('admin.layouts.app_admin')
+
+
+@section('content')
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-3" style="background-color: black">First</div>
+        <div class="col-md-3" style="background-color: green">Second</div>
+        <div class="col-md-3" style="background-color: #123c24">Third</div>
+        <div class="col-md-3" style="background-color: #1b4b72">Fourth</div>
+    </div>
+</div>
+@endsection
